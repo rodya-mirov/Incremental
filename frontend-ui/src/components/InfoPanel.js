61ec2dca8fe9965fde3f9c370cc1bab539addae3
Richard Rast <Richard.Rast@trinetx.com>
2017-12-12 18:44:52 -0500

    Upkeep now occurs, is visible.

--- frontend-ui/src/components/InfoPanel.js
+++ frontend-ui/src/components/InfoPanel.js
@@ -5,13 +5,13 @@ import bigInt from 'big-integer';
 /**
  * Panel for displaying current assets.
  */
-const InfoPanel = ({ money, widgets, workerDrones, salesDrones }) => (
+const InfoPanel = ({ money, widgets, workerDrones, salesDrones, workerCostPerTick, salesCostPerTick }) => (
   <table className="infoPanel-table">
     <tbody>
-      <tr><td>Money:</td><td>{money.toString()}</td></tr>
+      <tr><td>Money:</td><td>${money.toString()}</td></tr>
       <tr><td>Widgets:</td><td>{widgets.toString()}</td></tr>
-      <tr><td>Worker Drones:</td><td>{workerDrones.toString()}</td></tr>
-      <tr><td>Sales Drones:</td><td>{salesDrones.toString()}</td></tr>
+      <tr><td>Worker Drones:</td><td>{workerDrones.toString()}</td><td>Cost/Tick:</td><td>${workerCostPerTick.toString()}</td></tr>
+      <tr><td>Sales Drones:</td><td>{salesDrones.toString()}</td><td>Cost/Tick:</td><td>${salesCostPerTick.toString()}</td></tr>
     </tbody>
   </table>
 );
@@ -19,8 +19,12 @@ const InfoPanel = ({ money, widgets, workerDrones, salesDrones }) => (
 InfoPanel.propTypes = {
   money: PropTypes.instanceOf(bigInt).isRequired,  // amount of money owned
   widgets: PropTypes.instanceOf(bigInt).isRequired, // amount of widgets owned
+
   workerDrones: PropTypes.instanceOf(bigInt).isRequired,
+  workerCostPerTick: PropTypes.instanceOf(bigInt).isRequired,
+
   salesDrones: PropTypes.instanceOf(bigInt).isRequired,
+  salesCostPerTick: PropTypes.instanceOf(bigInt).isRequired,
 };
 
 export default InfoPanel;
